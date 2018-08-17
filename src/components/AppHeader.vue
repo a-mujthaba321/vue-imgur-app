17e652182c5af920ea280aa67693eaa8ad8a950b
Ali Mujthaba <a.mujthaba321@gmail.com>
2018-08-17 10:18:19 +0500

    Added favicon and title and styling to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    Image Storage
+  <div class="ui secondary pointing menu">
+    <a 
+      href="/" 
+      class="active item">
+      Image Storage
+    </a>
   </div>
 </template>
 
